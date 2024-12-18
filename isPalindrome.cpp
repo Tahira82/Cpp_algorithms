9974381fdd06a8f0e45af90f991082393f2eac9a
tahira <tahira@tu-ilmenau.de>
2024-12-18 14:34:46 +0100

    is palindrome using inbuilt function

--- isPalindrome.cpp
+++ isPalindrome.cpp
@@ -4,6 +4,40 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+bool isPalindromeInbuilt(string s) {
+
+    //RunTime complexity = O(n)
+    // better memory (space complexity) O(1)
+    int start = 0;
+    int end = s.length() -1;
+    bool isPal = true;
+
+    while(end>start)
+    {
+        if (!isalnum(tolower(s[start]))){
+
+            start++;
+            continue;
+        }
+        if (!isalnum(tolower(s[end]))){
+
+            end--;
+            continue;
+        }
+
+        if (tolower(s[start]) != tolower(s[end]))
+        {
+            isPal = false;
+            return isPal;
+        }
+
+        start++;
+        end--;
+    }
+    return isPal;
+}
+
 //problem: ignore non-alpha-numeric+uppercase to lowercase conversion, s from 1 to 2*10^5
 bool isPalindrome(string s) {
 
@@ -44,5 +78,6 @@ bool isPalindrome(string s) {
 int main(){
   string s = "A man, a plan, a canal: Panama";
   cout << isPalindrome(s) << endl;
+    cout << isPalindromeInbuilt(s) << endl;
   return 0;
 }
